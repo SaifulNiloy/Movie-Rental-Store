aabad6ac75c24f742f20074bae62a07237994523
SaifulNiloy <saifulniloy25@gmail.com>
2018-12-13 21:25:50 +0600

    add all files

---
+++ MovieRentalStore/Controllers/MoviesController.cs
@@ -0,0 +1,79 @@
+﻿using MovieRentalStore.Models;
+using MovieRentalStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieRentalStore.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "S"},
+                new Movie {Id = 2, Name = "W"}
+            };
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek"
+            };
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            //ViewData["Movie"] = movie;
+            //ViewBag.Movie = movie;
+            return View(viewModel);
+
+        }
+
+        //[Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
+
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("Id= " + id);
+        //}
+
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //    {
+        //        pageIndex = 1;
+        //    }
+        //    if (String.IsNullOrWhiteSpace(sortBy)) {
+        //        sortBy = "Name";
+        //    }
+        //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+
+        //}
+    }
+}
