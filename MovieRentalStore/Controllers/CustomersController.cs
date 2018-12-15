87d5f4395837e3da86a2a3c791c71119acb92932
SaifulNiloy <saifulniloy25@gmail.com>
2018-12-15 18:07:33 +0600

    fixing the ActionLink in Index.chtml in Customer folder for Customers/Details/1 route

--- MovieRentalStore/Controllers/CustomersController.cs
+++ MovieRentalStore/Controllers/CustomersController.cs
@@ -9,18 +9,30 @@ namespace MovieRentalStore.Controllers
 {
     public class CustomersController : Controller
     {
+
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            //base.Dispose(disposing);
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
             return View(customers);
         }
 
         public ActionResult Details (int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
-            if(customer == null)
+            if (customer == null)
             {
                 return HttpNotFound();
             }
@@ -28,13 +40,13 @@ namespace MovieRentalStore.Controllers
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer {Id = 1, Name = "J S"},
-                new Customer {Id = 2, Name = "M W"}
-            };
-        }
+        //private IEnumerable<Customer> GetCustomers()
+        //{
+        //    return new List<Customer>
+        //    {
+        //        new Customer {Id = 1, Name = "J S"},
+        //        new Customer {Id = 2, Name = "M W"}
+        //    };
+        //}
     }
 }
