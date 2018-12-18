3fa8acabfe4b81b5bcec4d4b1d8b5b029796e13c
SaifulNiloy <saifulniloy25@gmail.com>
2018-12-18 14:54:52 +0600

    Edit a customer

--- MovieRentalStore/Controllers/CustomersController.cs
+++ MovieRentalStore/Controllers/CustomersController.cs
@@ -26,11 +26,11 @@ namespace MovieRentalStore.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel()
+            var viewModel = new CustomerFormViewModel()
             {
                 MembershipTypes = membershipTypes
             };
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
@@ -58,6 +58,23 @@ namespace MovieRentalStore.Controllers
             }
 
             return View(customer);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new CustomerFormViewModel()
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm", viewModel);
         }
 
         //private IEnumerable<Customer> GetCustomers()
