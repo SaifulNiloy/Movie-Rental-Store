03ebca2f679a8cc608a28b40168d73194c1de97e
SaifulNiloy <saifulniloy25@gmail.com>
2019-01-02 21:07:27 +0600

    add MovieForm

---
+++ MovieRentalStore/Migrations/201901021132379_MovieForm.cs
@@ -0,0 +1,16 @@
+namespace MovieRentalStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MovieForm : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
