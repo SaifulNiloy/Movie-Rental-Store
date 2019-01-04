5cc0a54ae72e6dd9d96635403f67125044e0ee4a
SaifulNiloy <saifulniloy25@gmail.com>
2019-01-04 07:06:35 +0600

    add custom validation for Birthdate

--- MovieRentalStore/Models/Customer.cs
+++ MovieRentalStore/Models/Customer.cs
@@ -10,7 +10,7 @@ namespace MovieRentalStore.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Enter Customer's Name")]
         [StringLength(255)]
         public string Name { get; set; }
 
@@ -22,6 +22,7 @@ namespace MovieRentalStore.Models
         public byte MembershipTypeId { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
     }
 }
